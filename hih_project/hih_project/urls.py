11696a6906c4e9ec83e8fe0de073531abd99aba6
SlowProgramm <arturzlo13@gmail.com>
2025-11-13 21:32:13 +0300

    test commit

---
+++ hih_project/hih_project/urls.py
@@ -0,0 +1,10 @@
+
+
+from django.contrib import admin
+from django.urls import path
+from main.views import index, about
+
+urlpatterns = [
+    path('', index, name = 'home'),
+    path('about/', about, name = 'about'),
+]
