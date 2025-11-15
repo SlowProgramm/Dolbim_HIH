d5f649bbf2a88017636da6f1ed26801cd9293e34
Nedelis <dimoshunya@gmail.com>
2025-11-15 16:06:06 +0300

    top 5 edits

--- hih_project/hih_project/urls.py
+++ hih_project/hih_project/urls.py
@@ -5,15 +5,14 @@ from django.conf.urls.static import static
 from main.views import *
 
 urlpatterns = [
-    path('', index_view, name='home'),
-    path('about/', about_view, name='about'),
+    path('', index_view, name='index'),
     path('signup/', signup_view, name='signup'),
     path('login/', login_view, name='login'),
     path('account/', account_view, name='account'),
     path('admin/', admin.site.urls),
     path('apps/', apps_view, name='apps'),
     path('app/<str:app_id>/', app_detail_view, name='app_detail'), 
-    path('categories/', categories_view, name='category_list'),
+    path('categories/', categories_view, name='categories'),
     path('app_for_category/', apps_for_category_view, name='app_for_category'),
     path('developer/<str:dev_id>/', developer_view, name='developer'),
 ]
